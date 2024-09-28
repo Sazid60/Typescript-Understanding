b3d6a17cdc73272fde3f806f51c975e794eb178a
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-28 13:22:45 +0600

    Type-Alias

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -191,3 +191,18 @@ function printUserName(person: { firstName: string; lastName: string }) {
 
 printUserName({ firstName: "HuXn", lastName: "WebDev" });
 // -------------------------
+
+// Type-Alias
+type User = {
+  name: string;
+  age: number;
+  location: string;
+};
+
+const printUserInfo = (user: User) => {
+  return `Name: (${user.name}) Age: (${user.age}) Location: (${user.location})`;
+};
+
+const result1 = printUserInfo({ name: "Alex", age: 20, location: "USA" });
+console.log(result1); 
+
