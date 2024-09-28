70d2e91a990c664dd3dd49c35a7f5de1439582c2
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-28 12:39:07 +0600

    Array Types

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -114,3 +114,34 @@ function neverReturns(): never {
 }
 
 x = neverReturns(); // This line will cause a compile-time error because the function never returns
+
+
+// Arrays Type 
+// -------------------------
+const nums: number[] = [1, 2, 3, 4, 5];
+console.log(nums);
+
+const str: string[] = ["one", "two", "three"];
+console.log(str);
+// -------------------------
+
+// -------------------------
+const items: string[] = [];
+items.push("Remote");
+items.push("Keyboard");
+// items.push(21); // NO nO
+console.log(items);
+
+const numList: number[] = [];
+numList.push(20);
+numList.push(2000);
+numList.push(-1);
+// numList.push("Hello") // ERROR
+console.log(numList);
+// -------------------------
+
+const items3: Array<string> = [];
+// const items3: string[] = [];
+
+const items4: Array<number> = [];
+// const items4: numbers[] = []
