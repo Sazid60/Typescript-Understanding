2eb0f9151c0c80baffb7ba47d1f9d78b5b3c14c7
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-28 11:54:22 +0600

    Type Inference

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -13,10 +13,26 @@ myName = "Another Name"
 console.log(myName)
 
 // Annotation of Number
-let favNumber : number = 7;
-favNumber += 2
-console.log(favNumber)
+// let favNumber : number = 7;
+// favNumber += 2
+// console.log(favNumber)
 
 // Boolean Annotation
 let isHard : boolean = false;
 console.log(isHard)
+
+// ____________ Type inference
+// In TypeScript, type inference refers to the compiler's ability to automatically determine the type of a variable or expression based on how it is initialized or used, without the need for explicit type annotations.
+
+// Here we're inferring the types!
+let tech = "TypeScript";
+let favNumber = 8;
+let tsHard = true;
+
+console.log(tech, favNumber, tsHard);
+
+// ERROR
+// tech = false
+// favNumber = "Hello"
+// tsHard = 20
+
