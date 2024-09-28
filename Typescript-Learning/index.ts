cc5c5b3b064037196f0b437522881e9be027a312
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-28 12:21:54 +0600

    Return Annotation

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -71,3 +71,17 @@ function greet(person: string = "Anonymous") {
   
   const res1 = greet();
   console.log(res1);
+
+//   Return Annotation 
+// Regular Function
+function double1(x: number): number {
+    return x * x;
+  }
+  
+  const res2 = double1(2);
+  console.log(res2);
+  
+  // Using Arrow Functions
+  const double2 = (x: number): number => x * x;
+  const res3 = double2(2);
+  console.log(res3);
