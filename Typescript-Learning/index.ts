f4609845798f14f532af61aa4569be44abbffbd8
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-28 11:18:56 +0600

    Annotation Understanding

---
+++ Typescript-Learning/index.ts
@@ -0,0 +1,22 @@
+type Person = {
+    name: string;
+};
+
+const person : Person = {
+    name:"Jon",
+};
+console.log(person.name)
+
+// Annotation of string
+let myName : string = "Sazid";
+myName = "Another Name"
+console.log(myName)
+
+// Annotation of Number
+let favNumber : number = 7;
+favNumber += 2
+console.log(favNumber)
+
+// Boolean Annotation
+let isHard : boolean = false;
+console.log(isHard)
