e617203e847f7e70c6dca812b901706e48eae901
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-28 13:37:31 +0600

    Intersection Tpes or Combined Types

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -238,3 +238,29 @@ const user7: Person2 = {
 
 console.log(user7.email); // Valid
 // user.email = "john@gmail.com" // 🚫 Not Valid
+
+// Intersection Type
+type UserInfo = {
+  first: string;
+  last: string;
+  age: number;
+};
+
+type AccountDetails = {
+  email: string;
+  password: string;
+};
+
+type User5 = UserInfo & AccountDetails;
+
+const huxn: User5 = {
+  first: "HuXn",
+  last: "WebDev",
+  age: 18,
+  email: "test@gmail.com",
+  password: "strongpassword123",
+};
+
+console.log(
+  `Name: (${huxn.first} ${huxn.last}) Age: (${huxn.age}) Email: (${huxn.email}) Password: (${huxn.password})`
+);
