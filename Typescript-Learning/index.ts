098baf6a21f4234c0e6e23fa6d81ca0d6bda942c
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-29 10:24:51 +0600

    Extending Interfaces

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -463,3 +463,27 @@ const song1: Song = {
 
 console.log(song1.printSongInfo("Natural", "Imagin Drigon"));
 // ---------------------------
+
+// Extending Interfaces
+// Interface #5 (Extending Interface)
+interface MovieDetails {
+  readonly name: string;
+  ratings: number;
+  printMovieInfo(name: string, price: number, ratings: number): string | number;
+}
+
+interface MovieGenra extends MovieDetails {
+  genra: string;
+}
+
+const movie3: MovieGenra = {
+  name: "John Wick",
+  ratings: 7,
+  printMovieInfo(name: string, price: number, ratings: number) {
+    return `Name: ${name}, Price: ${price}, Ratings: ${ratings}`;
+  },
+  genra: "Action",
+};
+
+const res5 = movie3.printMovieInfo("John Wick", 100, 8);
+console.log(res5);
