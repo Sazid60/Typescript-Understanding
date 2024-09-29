4502672165193234ac027fab52ce0c8c4b2657d0
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-29 09:14:29 +0600

    Inerfaces In Type Script

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -325,3 +325,44 @@ enum WeatherConditions {
 
 const currentWeather = WeatherConditions.Sunny;
 console.log(`The current weather is ${currentWeather}`);
+
+// Interfaces
+//  Interfaces is a way to define a cntract for the shape of an object. it specifies the properties and their types that an object must have. Interfaces are a powerful toool for enforcing a certain structure in your Code.
+interface Computer {
+  name: string;
+  ram: number;
+  hdd: number;
+}
+
+// Usage
+const computerExample: Computer = {
+  name: "i7",
+  ram: 16,
+  hdd: 23000,
+};
+
+console.log(computerExample.name);
+console.log(computerExample.ram);
+console.log(computerExample.hdd);
+// ---------------------------
+
+// ----------------------------------
+// Interface #1 (Simple Interface)
+interface Movie {
+  readonly name: string; // 👈 Readonly Property
+  ratings: number;
+  genra?: string; // 👈 Optional Property
+}
+
+const movie1: Movie = {
+  name: "John Wick",
+  ratings: 7,
+  genra: "Action",
+};
+
+// movie1.name = "Another Movie" // 👈 ERROR
+console.log(movie1);
+// ----------------------------------
+
+
+
