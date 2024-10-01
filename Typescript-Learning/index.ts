94d91dbc8b182f21b3b503485684013bb3cf48c5
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-01 15:31:35 +0600

    instance of Type Narrowing

--- Typescript-Learning/index.ts
+++ Typescript-Learning/index.ts
@@ -681,3 +681,36 @@ function exampleFunction(value: MyType): void {
 // Example usage
 exampleFunction("hello"); // Outputs: HELLO
 exampleFunction(42); // Outputs: 42.00
+
+
+// Type Narrowing Using instanceof: this instance of operator is another type guard in typescript that allows you to check whether an object is an instance of a particular class or constructor function   
+
+class Dog {
+  bark(): void {
+    console.log("Woof!");
+  }
+}
+
+class Cat {
+  meow(): void {
+    console.log("Meow!");
+  }
+}
+
+// Example function with instanceof type guard
+function animalSound(animal: Dog | Cat): void {
+  if (animal instanceof Dog) {
+    // Within this block, TypeScript knows that 'animal' is an instance of Dog
+    animal.bark();
+  } else {
+    // Within this block, TypeScript knows that 'animal' is an instance of Cat
+    animal.meow();
+  }
+}
+
+// Example usage
+const myDog = new Dog();
+const myCat = new Cat();
+
+animalSound(myDog); // Outputs: Woof!
+animalSound(myCat); // Outputs: Meow!
